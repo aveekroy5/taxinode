f134304b9125054c4b598b5626d06c17da8a4c93
Aveek Roy <aveekroy5@gmail.com>
2020-07-01 01:25:01 +0530

    2nd commit

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -4,6 +4,9 @@ var nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: 'aveekroy55555@gmail.com',
     pass: 'loveis420'
